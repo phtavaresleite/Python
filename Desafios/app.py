b468a8d3bcd58ec58efdbffae199de6a3bb3db2b
Paulo Henrique <146396832+phtavaresleite@users.noreply.github.com>
2023-10-09 15:19:04 -0300

    Update app.py
    Otimizei o código utilizando um dicionário para realizar a busca do cupom promocional.

--- Desafios/app.py
+++ Desafios/app.py
@@ -1,11 +1,9 @@
 valor_produto = float(200)
-
 codigo_promocional = input("Digite o seu código promocional:" )
 
-if codigo_promocional == "VBFMZB":
-    valor_desconto = valor_produto-valor_produto*0.25
-    print(valor_desconto)
-elif codigo_promocional == "HT2Y8E" :
+Cupons = ["VBFMZB","HT2Y8E"]
+
+if codigo_promocional.upper() in Cupons:
     valor_desconto = valor_produto-valor_produto*0.25
     print(valor_desconto)
 else:
